16ed3fb703acd6c1244553bd34800c5ef852c259
amir-zeldes <amir.zeldes@georgetown.edu>
2016-09-29 15:56:47 -0400

    PyPI restructure   * Restructured directories for PyPI distribution

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'depedit',
+  packages = ['depedit'],
+  version = '1.5.1',
+  description = 'A simple configurable tool for manipulating dependency trees',
+  author = 'Amir Zeldes',
+  author_email = 'amir.zeldes@georgetown.edu',
+  url = 'https://github.com/amir-zeldes/depedit', 
+  download_url = 'https://github.com/amir-zeldes/depedit/tarball/1.5.1',
+  keywords = ['NLP', 'parsing', 'syntax', 'dependencies', 'dependency', 'tree', 'treebank'],
+  classifiers = [],
+)
