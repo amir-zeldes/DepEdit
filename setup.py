3fed0a2e59249ecbbc3c1ffb28f90070be245a5d
amir-zeldes <amir.zeldes@georgetown.edu>
2016-09-29 16:44:58 -0400

    V1.6.1   * Updated license in setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ from distutils.core import setup
 setup(
   name = 'depedit',
   packages = ['depedit'],
-  version = '1.6.0',
+  version = '1.6.1',
   description = 'A simple configurable tool for manipulating dependency trees',
   author = 'Amir Zeldes',
   author_email = 'amir.zeldes@georgetown.edu',
   url = 'https://github.com/amir-zeldes/depedit', 
+  license='Apache License, Version 2.0',
   download_url = 'https://github.com/amir-zeldes/depedit/tarball/1.5.1',
   keywords = ['NLP', 'parsing', 'syntax', 'dependencies', 'dependency', 'tree', 'treebank'],
   classifiers = ['Programming Language :: Python',
 'Programming Language :: Python :: 2',
 'Programming Language :: Python :: 3',
+'License :: OSI Approved :: Apache Software License',
 'Operating System :: OS Independent'],
 )
