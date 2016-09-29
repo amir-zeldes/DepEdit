4175910774394929f8367285c3e66997cd797516
amir-zeldes <amir.zeldes@georgetown.edu>
2016-09-29 16:45:50 -0400

    Typo in setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author_email = 'amir.zeldes@georgetown.edu',
   url = 'https://github.com/amir-zeldes/depedit', 
   license='Apache License, Version 2.0',
-  download_url = 'https://github.com/amir-zeldes/depedit/tarball/1.5.1',
+  download_url = 'https://github.com/amir-zeldes/depedit/tarball/1.6.1',
   keywords = ['NLP', 'parsing', 'syntax', 'dependencies', 'dependency', 'tree', 'treebank'],
   classifiers = ['Programming Language :: Python',
 'Programming Language :: Python :: 2',
