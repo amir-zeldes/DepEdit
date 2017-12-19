db719bdd0102a2a301e8b1a5ec622e21695fd0f3
amir-zeldes <amir.zeldes@georgetown.edu>
2017-12-19 11:34:59 -0500

    Update to V1.8.0

--- depedit/depedit.py
+++ depedit/depedit.py
@@ -19,7 +19,7 @@ from collections import defaultdict
 from glob import glob
 from six import iteritems
 
-__version__ = "DEVELOP"
+__version__ = "1.8.0"
 
 
 def escape(string,symbol_to_mask,border_marker):
