07c23d3e6dab3f1cc2a57fda2a48e05ac9d470cc
amir-zeldes <amir.zeldes@georgetown.edu>
2017-12-19 11:10:50 -0500

    Add support for conllu ellipsis tokens   * IDs like 10.1 are now supported   * NB: distance between IDs 10 and 11 is still considered 1, even if ellipsis tokens intervene   * Ellipsis token ID distances are rounded down (so 10.1 is one token away from 11, and so is 10.2 ... 10.9)   * Fixes #7

--- depedit/depedit.py
+++ depedit/depedit.py
@@ -138,8 +138,8 @@ class Transformation:
 			criteria = node.split("&")
 			criteria = (_crit.replace("%%%%%","&") for _crit in criteria)
 			for criterion in criteria:
-				if not re.match("(text|pos|cpos|lemma|morph|func|head|func2|head2|num|form|upostag|xpostag|feats|deprel|deps|misc)!?=/[^/=]*/",criterion):
-					if not re.match(r"position!?=/first|last|mid/",criterion):
+				if re.match("(text|pos|cpos|lemma|morph|func|head|func2|head2|num|form|upostag|xpostag|feats|deprel|deps|misc)!?=/[^/=]*/",criterion) is None:
+					if re.match(r"position!?=/(first|last|mid)/",criterion) is None:
 						report+= "Invalid node definition in column 1: " + criterion
 		for relation in self.relations:
 			if relation == "none" and len(self.relations) == 1:
@@ -385,8 +385,8 @@ class DepEdit():
 		elif "==" in relation:
 			node1 = relation.split(operator)[0]
 			node2 = relation.split(operator)[1]
-			node1=int(node1.replace("#", ""))
-			node2=int(node2.replace("#", ""))
+			node1=float(node1.replace("#", ""))
+			node2=float(node2.replace("#", ""))
 
 			for matcher1 in node_matches[node1]:
 				tok1 = matcher1.token
@@ -409,8 +409,8 @@ class DepEdit():
 			node1 = relation.split(operator)[0]
 			node2 = relation.split(operator)[1]
 
-			node1=int(node1.replace("#", ""))
-			node2=int(node2.replace("#", ""))
+			node1=float(node1.replace("#", ""))
+			node2=float(node2.replace("#", ""))
 			for matcher1 in node_matches[node1]:
 				tok1 = matcher1.token
 				for matcher2 in node_matches[node2]:
@@ -437,12 +437,12 @@ class DepEdit():
 	@staticmethod
 	def test_relation(node1,node2,operator):
 		if operator == ".":
-			if int(node2.id) == int(node1.id)+1:
+			if int(float(node2.id)) == int(float(node1.id))+1:
 				return True
 			else:
 				return False
 		elif operator == ">":
-			if int(node2.head) == int(node1.id):
+			if int(float(node2.head)) == int(float(node1.id)):
 				return True
 			else:
 				return False
@@ -454,13 +454,13 @@ class DepEdit():
 					max_dist = int(m.group(2).replace(",",""))
 				else:
 					max_dist = min_dist
-				if max_dist >= int(node2.id) - int(node1.id) >= min_dist:
+				if max_dist >= int(float(node2.id)) - int(float(node1.id)) >= min_dist:
 					return True
 				else:
 					return False
 			else:
 				dist = int(m.group(1))
-				if int(node2.id) - int(node1.id) == dist:
+				if int(float(node2.id)) - int(float(node1.id)) == dist:
 					return True
 				else:
 					return False
@@ -715,10 +715,13 @@ class DepEdit():
 				tok_id = tok.id
 			elif tok.head == "0":
 				tok_head_string = "0"
-				tok_id = str(int(tok.id) - tokoffset)
+				tok_id = str(float(tok.id) - tokoffset)
 			else:
-				tok_head_string = str(int(tok.head)-tokoffset)
-				tok_id = str(int(tok.id)-tokoffset)
+				tok_head_string = str(float(tok.head)-tokoffset)
+				tok_id = str(float(tok.id)-tokoffset)
+			# Only keep decimal ID component for non-0 ellipsis IDs, e.g. 10.1
+			tok_id = tok_id.replace(".0","")
+			tok_head_string = tok_head_string.replace(".0","")
 			if self.input_mode == "8col":
 				output_tree += tok_id+"\t"+tok.text+"\t"+tok.lemma+"\t"+tok.pos+"\t"+tok.cpos+"\t"+tok.morph+\
 								"\t"+tok_head_string+"\t"+tok.func+"\n"
@@ -781,12 +784,12 @@ class DepEdit():
 					head_id = cols[6]
 				else:
 					super_tok = False
-					tok_id = str(int(cols[0]) + tokoffset)
+					tok_id = str(float(cols[0]) + tokoffset)
 					if cols[6] == "_":
 						sys.stderr.write("DepEdit WARN: head not set for token " + tok_id + " in " + filename + "\n")
 						head_id = str(0 + tokoffset)
 					else:
-						head_id = str(int(cols[6]) + tokoffset)
+						head_id = str(float(cols[6]) + tokoffset)
 				if len(cols) > 8:
 					# Collect token from line; note that head2 is parsed as a string, which is often "_" for monoplanar trees
 					this_tok = ParsedToken(tok_id, cols[1], cols[2], cols[3], cols[4], cols[5],head_id, cols[7].strip(), cols[8], cols[9].strip(), cols[0], [], "mid",super_tok)
@@ -799,8 +802,8 @@ class DepEdit():
 				conll_tokens.append(this_tok)
 				if not super_tok:
 					sentlength += 1
-					children[str(int(head_id) + tokoffset)].append(tok_id)
-					child_funcs[(int(head_id) + tokoffset)].append(cols[7])
+					children[str(float(head_id) + tokoffset)].append(tok_id)
+					child_funcs[(float(head_id) + tokoffset)].append(cols[7])
 				else:
 					supertok_length += 1
 
