640d6c0b25f6e197ab9ebe991613ca0b85ed11f0
amir-zeldes <amir.zeldes@georgetown.edu>
2018-07-10 18:47:00 -0400

    Version 2.1.2

--- depedit/depedit.py
+++ depedit/depedit.py
@@ -19,7 +19,7 @@ from collections import defaultdict
 from glob import glob
 from six import iteritems
 
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 
 
 def escape(string,symbol_to_mask,border_marker):
